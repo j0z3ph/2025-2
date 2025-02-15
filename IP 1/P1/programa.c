dcaa02608e11152dc70d5f4c05eb9e62d5ea9b66
Jose Luis Cruz <j0z3ph@gmail.com>
2025-02-14 20:28:13 -0600

    Mi primer programa

---
+++ IP 1/P1/programa.c
@@ -0,0 +1,21 @@
+/**
+ * @file programa.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C :>
+ * @version 0.1
+ * @date 2025-02-14
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include <stdio.h>
+
+int main()
+{
+    putc(getc(stdin), stdout);
+    putc(getc(stdin), stdout);
+    putc(getc(stdin), stdout);
+    
+
+    return 0;
+}
