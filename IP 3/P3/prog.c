7db6c130d66fa7cd652031e38f90f5cb07b79da5
Jose Luis Cruz <j0z3ph@gmail.com>
2025-02-21 20:26:29 -0600

    Ejemplos

---
+++ IP 3/P3/prog.c
@@ -0,0 +1,22 @@
+/**
+ * @file prog.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief 
+ * @version 0.1
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include<stdio.h>
+#define PI 3.1416
+
+int main()
+{
+    //static int variable_local = 7;
+    //const float PI = 3.1416;
+
+    printf("%f\n", PI);
+    
+    return 0;
+}
---
+++ PE/P3/progama.c
@@ -0,0 +1,22 @@
+/**
+ * @file progama.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Constantes y mas
+ * @version 0.1
+ * @date 2025-02-21
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include<stdio.h>
+#define PI 3.1416
+
+int main()
+{
+    //static int variale_local = 1;
+    //const float PI = 3.1416;
+
+    printf("%f\n", PI);
+    
+    return 0;
+}
