746fb658e8e47a01cc014e809bb3803551a02fda
Jose Luis Cruz <j0z3ph@gmail.com>
2025-06-05 20:27:06 -0600

    Hola

---
+++ ADP 5/programa.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file programa.cpp
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C++
+ * @version 0.1
+ * @date 2025-06-05
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include<iostream>
+
+int main()
+{
+    printf("Holi canoli\n");
+    
+    return 0;
+}
