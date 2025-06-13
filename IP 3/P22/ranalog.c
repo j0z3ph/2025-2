59c3879eedbccac9ccf7b0ca2652bcf422441b7b
Jose Luis Cruz <j0z3ph@gmail.com>
2025-06-13 17:23:49 -0600

    Comunicacion serial

---
+++ IP 3/P22/ranalog.c
@@ -0,0 +1,27 @@
+/**
+ * @file ranalog.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief 
+ * @version 0.1
+ * @date 2025-06-13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include<stdio.h>
+#include"simplecontroller.h"
+
+int main()
+{
+    Board *dev = connectDevice("COM3", B115200);
+
+    dev->pinMode(dev, 15, INPUT);
+
+    while(true) {
+        printf("%f\n", dev->analogRead(dev, 15));
+    }
+
+    dev->closeDevice(dev);
+    
+    return 0;
+}
---
+++ IP 3/P22/wdigital.c
@@ -0,0 +1,17 @@
+#include "simplecontroller.h"
+
+int main()
+{
+    Board *esp32 = connectDevice("COM3", B115200);
+    esp32->pinMode(esp32, 2, OUTPUT);
+
+    while(true) {
+        esp32->digitalWrite(esp32, 2, true);
+        Sleep(1000);
+        esp32->digitalWrite(esp32, 2, false);
+        Sleep(1000);
+    }
+
+    esp32->closeDevice(esp32);
+    return 0;
+}
