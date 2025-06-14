07da9d1c08f3df714cb3b39eb5ea49e250914305
Jose Luis Cruz <j0z3ph@gmail.com>
2025-06-13 20:20:32 -0600

    Puertos

---
+++ IP 1/P22/wdigital.c
@@ -0,0 +1,28 @@
+/**
+ * @file wdigital.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief 
+ * @version 0.1
+ * @date 2025-06-13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include <stdio.h>
+#include "simplecontroller.h"
+
+int main()
+{
+    Board *esp32 = connectDevice("COM3", B115200);
+
+    esp32->pinMode(esp32, 2, OUTPUT);
+
+    while(true) {
+        esp32->digitalWrite(esp32, 2, true);
+        Sleep(1000);
+        esp32->digitalWrite(esp32, 2, false);
+        Sleep(1000);
+    }
+
+    return 0;
+}
